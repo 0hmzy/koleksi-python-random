3f79c9c8e20e65faf554ce8c906af9a824a7ff69
Abdurachman Romzy <49395558+0hmzy@users.noreply.github.com>
2022-06-15 13:08:02 +0700

    Add files via upload

---
+++ mini/password manager.py
@@ -0,0 +1,45 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key() 
+fer = Fernet(key)
+    
+def view():
+    with open("passwords.txt", "r") as f:
+       for line in f.readlines():
+          data = line.rstrip()
+          user, passw = data.split("|")
+          print("User:", user, "|  Password:", passw)
+          fer.decrypt(passw.encode().decode())
+
+def add():
+    name = input("Account name : ")
+    pwd = input("Password : ")
+    
+    with open("passwords.txt", "a") as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n" )
+
+while True:
+    mode = input("Would you like to add a new password or view the existing one (add, view)?" ).lower()
+    if mode == "q":
+        break
+    
+    elif mode == "add":
+        add()
+    elif mode == "view":
+        view()
+    else:
+        print("Invalid mode.")
+        continue
+    
